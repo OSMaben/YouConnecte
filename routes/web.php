3621370966c2a790fe52f9725d7052f02cf43d59
osmben <freegiftcardishere@gmail.com>
2024-02-13 20:35:02 +0100

    adding the messages page

--- routes/web.php
+++ routes/web.php
@@ -28,4 +28,8 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::get('/messages', function () {
+    return view('messages');
+});
+
 require __DIR__.'/auth.php';
